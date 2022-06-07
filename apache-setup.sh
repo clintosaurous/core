7322eb6a2a23db910f442488d02cafc2c3ab0966
The Dude <thedude@clintosaurous.com>
2022-06-07 10:28:27 -0400

    Updated Logging and Opts & Documentation
    -   Updated script logging to use the Clintosaurous logging
        commands. This includes outputing command output to logging
        commands.
    
    -   Removed CLI options for specifying the username and user group.
        -   This needs to be in the tools configuration file.
    
    -   Updated inline documentation.

--- apache-setup.sh
+++ apache-setup.sh
@@ -4,11 +4,11 @@
 # tools.
 #
 # Version: 1.0.0
-# Last Updated: 2022-06-02
+# Last Updated: 2022-06-07
 #
 # Change Log:
 #   v1.0.0:
-#       Initial creation. (2022-05-02)
+#       Initial creation. (2022-06-07)
 #
 # Note: See repository commit logs for change details.
 #
@@ -20,19 +20,27 @@
 #
 # Installation flow:
 #
-#   1.  Ensure the script is running on an Ubuntu 20.04 or 22.04 system. This
-#       check can be suppressed with CLI options.
-#   2.  Ensure the script is being ran as root user.
-#   3.  apt update and upgrade. This can be suppressed with CLI options.
-#   4.  Install required aptitude packages.
-#   7.  Update Clintosaurous core environment repository.
-#   9.  Create default Apache configuration file if needed.
-#   14. Install required PIP modules.
+#   1.  Parse configuration file to get Clintosaurous username and group.
+#   2.  Ensure the script is running on a supported OS. This check can be
+#       suppressed with CLI options.
+#   3.  Ensure the script is being ran as root user.
+#   4.  apt update and upgrade. This can be suppressed with CLI options.
+#   5.  Install required aptitude packages. This can be suppressed with CLI
+#       options.
+#   6.  Update Clintosaurous core environment repository. This can be
+#       suppressed with CLI options.
+#   7.  Create default Apache configuration file if needed.
+#   9.  Add default SSL certificate and key files if needed.
+#   10. Enable Clintosaurous Apache site if needed.
+#   11. Create Clintosaurous user Apache account.
+#   12. Add default web index page configuration if needed.
+#   13. Restart Apache with updated configuration.
 
 
 # Environment setup.
 echo "#### Setting initial environment ####"
 APTUPDATE=1
+IGNOREOS=0
 REPOUPDATE=1
 USERHOME=/opt/clintosaurous
 COREHOME=$USERHOME/core
@@ -40,6 +48,9 @@ ETCDIR=/etc/clintosaurous
 CORECONF=$ETCDIR/clintosaurous.yaml
 KEYFILE=/etc/apache2/default-www.key
 CERTFILE=/etc/apache2/default-www.pem
+
+. $COREHOME/lib/sh/general.sh
+
 
 # Read default username and password from core configuration file.
 echo "Parsing core configuration file for default username and password"
@@ -55,16 +66,17 @@ except KeyError:
     True
 "`
 if [ $? -ne 0 ]; then
-    echo "Error parsing configuration file $CORECONF" >&2
+    log_msg "Error parsing configuration file $CORECONF" >&2
     exit 1
 fi
+
 for VALUE in $CONFUSER
 do
     if [ -z "$CLINTUSER" ]; then CLINTUSER=$VALUE
     else CLINTGROUP=$VALUE ; fi
 done
 if [ -z "$CLINTUSER" ] || [ -z "$CLINTGROUP" ]; then
-    echo "Parsing username or user group name from $CORECONF" >&2
+    log_msg "Parsing username or user group name from $CORECONF" >&2
     exit 1
 fi
 
@@ -84,14 +96,13 @@ sudo `basename $0` [-h | --help] \\
     [ -a | --server-admin \\
     [ -k | --ssl-key /key/file/path.key \\
     [ -c | --ssl-cert /cert/file/path.pem \\
-    [ -U | --user ] username \\
-    [ -G | --group ] group \\
     [ -I | --ignore-os
 
     -h | --help
         Display this help message.
     -A | --no-apt-update
-        Skip updating the aptitude repository and installing system updates.
+        Skip updating the aptitude repository and installing updates, and skip
+        trying to install required software
     -R | --no-repo-update
         Skip performing a git pull to update the existing repository.
     -S | --server-name
@@ -99,16 +110,9 @@ sudo `basename $0` [-h | --help] \\
     -a | --server-admin
         Email address of the server administratory.
     -k | --ssl-key
-        Path to HTTPS SSL key file. Default: $KEYFILE
+        Alternate path to HTTPS SSL key file. Default: $KEYFILE
     -c | --ssl-cert
-        Path to HTTPS SSL certificate file. Default: $CERTFILE
-    -U | --username
-        Username to use for the Clintosaurous tools. This must match on all
-        servers that run the Clintosaurous environment. It is recommended to
-        use the default username. Default: $CLINTUSER
-    -G | --group
-        User's group name. It is recommended use the default.
-        Default: $CLINTGROUP
+        Alternate path to HTTPS SSL certificate file. Default: $CERTFILE
     -I | --ignore-os
         Suppress checking OS and OS version. Although checks are suppressed,
         the command set only supports Debian based hosts.
@@ -135,10 +139,6 @@ do
         "--ssl-key") shift; KEYFILE=$1 ;;
         "-c") shift; CERTFILE=$1 ;;
         "--ssl-cert") shift; CERTFILE=$1 ;;
-        "-U") shift ; CLINTUSER=$1 ;;
-        "--user") shift; CLINTUSER=$1 ;;
-        "-G") shift ; CLINTGROUP=$1 ;;
-        "--group") shift ; CLINTGROUP=$1 ;;
         "-I") IGNOREOS=1 ;;
         "--ignore-os") IGNOREOS=1 ;;
         *) echo "Unknown CLI option $1" ; echo ; usage ;;
@@ -147,46 +147,28 @@ do
 done
 
 
-# Verify running on Ubuntu and supporte version.
-if [ -z "$IGNOREOS" ] && \
-    [ -z "`egrep -E 'Ubuntu\s+2[02].04' /etc/lsb-release 2>/dev/null`" ]
-then
-    echo "
-Clintosaurous tools only support running on Ubuntu systems. Only tested and
-supported on Ubuntu 20.04 and 22.04. It may work on older versions, but has
-not been tested and is not supported.
-
-Use -I or --ignore-os to override this error.
-" >&2
-    exit 1
-fi
-
-
-# Ensure we're logged in as root.
-if [ `whoami` != 'root' ]; then
-    echo "The installation must be ran as root." >&2
-    exit 1
-fi
-
+# Verify running on supported OS and running as root.
+if [ $IGNOREOS -ne 0]; then check_valid_os ; fi
+check_login_user root
 
 # Header.
-echo "#### Clintosaurous Apache initial setup starting ####"
+log_msg "#### Clintosaurous Apache initial setup starting ####"
 
 
 # Install required packages.
-echo "Installing required aptitude packages"
+log_msg "Installing required aptitude packages"
 if [ $APTUPDATE -ne 0 ]; then
-    echo "Updating system"
-    apt update && apt upgrade -y
+    log_msg "Updating system"
+    apt update -q && apt upgrade -q -y 2>&1 | log_stdin
     if [ $? -ne 0 ]; then
-        echo "Error updating aptitude packages" >&2
+        log_msg "Error updating aptitude packages" >&2
         exit 1
     fi
 
-    echo "Installing required aptitude packages"
-    apt install -y apache2 apache2-suexec-pristine
+    log_msg "Installing required aptitude packages"
+    apt install -q -y apache2 apache2-suexec-pristine 2>&1 | log_stdin
     if [ $? -ne 0 ]; then
-        echo "Error installing required aptitude packages" >&2
+        log_msg "Error installing required aptitude packages" >&2
         exit 1
     fi
 
@@ -198,17 +180,18 @@ if [ $APTUPDATE -ne 0 ]; then
     fi
 
     # Enable Apache modules.
-    echo "Enabling apache2 modules"
-    a2enmod auth_form cgi request rewrite session session_cookie ssl socache_shmcb
+    log_msg "Enabling apache2 modules"
+    a2enmod auth_form cgi request rewrite \
+        session session_cookie ssl socache_shmcb 2>&1 | log_stdin
     if [ $? -ne 0 ]; then
-        echo "Error enabling Apache required modules" >&2
+        log_msg "Error enabling Apache required modules" >&2
         exit 1
     fi
 
-    echo "Enabling Apache"
-    systemctl enable apache2
+    log_msg "Enabling Apache"
+    systemctl enable apache2 2>&1 | log_stdin
     if [ $? -ne 0 ]; then
-        echo "Error enabling Apache" >&2
+        log_msg "Error enabling Apache" >&2
         exit 1
     fi
 fi
@@ -218,30 +201,31 @@ fi
 if [ $REPOUPDATE -ne 0 ]; then
     if [ -e $COREHOME ]; then
         if [ -e $COREHOME/LICENSE ]; then
-            echo "Clintosaurous core directory directory exists"
-            echo "Ensuring up to date"
+            log_msg "Clintosaurous core directory directory exists"
             # Must be ran as Clintosaurous user.
-            su - -c "cd $COREHOME && git pull" $CLINTUSER
+            su - -c "cd $COREHOME && git pull" $CLINTUSER 2>&1 | log_stdin
             if [ $? -ne 0 ]; then
-                echo "Error updating Clintosaurous core repository" >&2
+                log_msg "Error updating Clintosaurous core repository" >&2
                 exit 1
             fi
         else
-            echo "Clintosaurous core directory exists, but is not from the" >&2
-            echo "GitHub repository!" >&2
+            MSG="Clintosaurous core directory exists, but is not from the"
+            MST="$MSG GitHub repository!"
+            log_msg "$MSG" >&2
             exit 1
         fi
     else
-        echo "Clintosaurous core envirnoment must be installed and setup." >&2
-        echo "Run the core installation (install.sh) and then retry." >&2
+        MSG="Clintosaurous core envirnoment must be installed and setup."
+        MSG="$MSG Run the core installation (install.sh) and then retry."
+        log_msg "$MSG" 1>&2
         exit 1
     fi
 fi
 
 
 # Ensure configuration file exists. If not, add it and enable it.
-if [ ! -e /etc/apache2/sites-available/clintosaurous.conf ]; then
-    echo
+A2CONF=/etc/apache2/sites-available/clintosaurous.conf
+if [ ! -e $A2CONF ]; then
     # Get server name for configuration file.
     if [ -z "$SERVERNAME" ]; then
         echo -n "Enter server FQDN: " ; read SERVERNAME
@@ -253,63 +237,59 @@ if [ ! -e /etc/apache2/sites-available/clintosaurous.conf ]; then
 
     # Disable the default configuration file.
     if [ -e /etc/apache2/sites-enabled/000-default.conf ]; then
-        echo "Disabling Apache default configuration"
-        a2dissite 000-default
+        log_msg "Disabling Apache default configuration"
+        a2dissite 000-default 2>&1 | log_stdin
         if [ $? -ne 0 ]; then
-            echo "Error diabling Apache default site" >&2
+            log_msg "Error diabling Apache default site" >&2
             exit 1
         fi
     fi
 
-    echo "Adding default configuration file"
-    cp $COREHOME/lib/defaults/apache2.conf \
-        /etc/apache2/sites-available/clintosaurous.conf
+    log_msg "Adding default configuration file"
+    cp $COREHOME/lib/defaults/apache2.conf $A2CONF 2>& | log_stdin
 
     if [ -e $CERTFILE ]; then
-        echo "Certificate exists"
+        log_msg "Certificate exists"
     else
-        echo "Adding default web certificate and key"
-        cp /opt/clintosaurous/core/lib/defaults/default-www.key $KEYFILE
-        cp /opt/clintosaurous/core/lib/defaults/default-www.pem $CERTFILE
-        chmod 600 $KEYFILE
-        chown $CLINTUSER:$CLINTGROUP $KEYFILE $CERTFILE
+        log_msg "Adding default web certificate and key"
+        cp /opt/clintosaurous/core/lib/defaults/default-www.key $KEYFILE \
+            2>& | log_stdin
+        cp /opt/clintosaurous/core/lib/defaults/default-www.pem $CERTFILE \
+            2>& | log_stdin
+        chmod 600 $KEYFILE 2>& | log_stdin
+        chown $CLINTUSER:$CLINTGROUP $KEYFILE $CERTFILE 2>& | log_stdin
     fi
 
-    echo "Setting Apache run time user"
+    log_msg "Setting Apache run time user"
     sed -Ei "s/APACHE_RUN_USER=.+/APACHE_RUN_USER=$CLINTUSER/" \
-        /etc/apache2/envvars
-    echo "Setting Apache run time group"
+        /etc/apache2/envvars 2>& | log_stdin
+    log_msg "Setting Apache run time group"
     sed -Ei "s/APACHE_RUN_GROUP=.+/APACHE_RUN_GROUP=$CLINTGROUP/" \
-        /etc/apache2/envvars
-    echo "Setting Apache server name"
-    sed -Ei "s/<<<server-name>>>/$SERVERNAME/" \
-        /etc/apache2/sites-available/clintosaurous.conf
-    echo "Setting Apache server admin"
-    sed -Ei "s/<<<server-admin>>>/$SERVERADMIN/" \
-        /etc/apache2/sites-available/clintosaurous.conf
-    echo "Setting Apache SSL key file"
-    sed -Ei "s|<<<key-file>>>|$KEYFILE|" \
-        /etc/apache2/sites-available/clintosaurous.conf
-    echo "Setting Apache SSL certificate file"
-    sed -Ei "s|<<<cert-file>>>|$CERTFILE|" \
-        /etc/apache2/sites-available/clintosaurous.conf
+        /etc/apache2/envvars 2>& | log_stdin
+    log_msg "Setting Apache server name"
+    sed -Ei "s/<<<server-name>>>/$SERVERNAME/" $A2CONF 2>& | log_stdin
+    log_msg "Setting Apache server admin"
+    sed -Ei "s/<<<server-admin>>>/$SERVERADMIN/" $A2CONF 2>& | log_stdin
+    log_msg "Setting Apache SSL key file"
+    sed -Ei "s|<<<key-file>>>|$KEYFILE|" $A2CONF 2>& | log_stdin
+    log_msg "Setting Apache SSL certificate file"
+    sed -Ei "s|<<<cert-file>>>|$CERTFILE|" $A2CONF 2>& | log_stdin
 
     if [ ! -e /etc/apache2/sites-enabled/clintosaurous.conf ]; then
-        echo "Enabling Clintosaurous configuration"
-        a2ensite clintosaurous
+        log_msg "Enabling Clintosaurous configuration"
+        a2ensite clintosaurous 2>& | log_stdin
         if [ $? -ne 0 ]; then
-            echo "Error enabling Apache clintosaurous site" >&2
+            log_msg "Error enabling Apache clintosaurous site" >&2
             exit 1
         fi
     fi
 
     PASSFILE=/etc/apache2/htpasswd
     if [ ! -e $PASSFILE ]; then
-        echo "Setting up Clintosaurous user for web access"
-        echo
-        htpasswd -c $PASSFILE $CLINTUSER
+        log_msg "Setting up Clintosaurous user for web access"
+        htpasswd -c $PASSFILE $CLINTUSER 2>& | log_stdin
         if [ $? -ne 0 ]; then
-            echo "Error creating Apache user $CLINTUSER" >&2
+            log_msg "Error creating Apache user $CLINTUSER" >&2
             exit 1
         fi
         chmod 660 $PASSFILE
@@ -319,37 +299,40 @@ fi
 
 # Validate default index configuration file exits.
 if [ -e $ETCDIR/www-index.yaml ]; then
-    echo "Default index page configuration exists"
+    log_msg "Default index page configuration exists"
 else
-    echo "Adding index page default configuration"
-    cp $COREHOME/lib/defaults/www-index.yaml $ETCDIR/
-    chown -R $CLINTUSER:$CLINTGROUP $ETCDIR/
-    chmod g+w,o= $ETCDIR/www-index.yaml
+    log_msg "Adding index page default configuration"
+    cp $COREHOME/lib/defaults/www-index.yaml $ETCDIR/ 2>& | log_stdin
+    chown -R $CLINTUSER:$CLINTGROUP $ETCDIR/ 2>& | log_stdin
+    chmod g+w,o= $ETCDIR/www-index.yaml 2>& | log_stdin
 fi
 
 
 APACHERELOAD=1
 if [ ! -e "$KEYFILE" ]; then
-    echo "SSL key file $KEYFILE does not exist" >&2
-    echo "Update Apache configuration with correct key location, or" >&2
-    echo "place key at $KEYFILE" >&2
+    MSG="SSL key file $KEYFILE does not exist. Update Apache configuration"
+    MSG="$MSG with correct key location, or place key at $KEYFILE."
+    log_msg "$MSG"
     APACHERELOAD=0
 fi
 if [ ! -e "$CERTFILE" ]; then
-    echo "SSL certificate file $CERTFILE does not exist" >&2
-    echo "Update Apache configuration with correct certificate" >&2
-    echo "location, or place key at $KEYFILE" >&2
+    MSG="SSL certificate file $CERTFILE does not exist. Update Apache"
+    MSG="$MSG configuration with correct certificate location, or place"
+    MSG="$MSG key at $KEYFILE."
+    log_msg "$MSG"
     APACHERELOAD=0
 fi
 
 if [ $APACHERELOAD -ne 0 ]; then
-    echo "Restarting Apache service"
-    systemctl restart apache2
+    log_msg "Restarting Apache service"
+    systemctl restart apache2 2>&1 | log_stdin
     if [ $? -ne 0 ]; then
-        echo "Error enabling Apache clintosaurous site" >&2
-        echo "Check Apache service status or error log for more details" >&2
+        MSG="Error enabling Apache clintosaurous site. Check Apache service"
+        MSG="$MSG status or error log for more details"
+        log_msg "$MSG" >&2
+        exit 1
     fi
 fi
 
 
-echo "#### Clintosaurous Apache setup complete ####"
+log_msg "#### Clintosaurous Apache setup complete ####"
